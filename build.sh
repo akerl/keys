d33084910040d6585ae608c5b65680e9ebc8de26
Les Aker <me@lesaker.org>
2017-11-11 18:02:10 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -2,21 +2,29 @@
 
 export MESSAGE="
 # Generated from https://github.com/akerl/keys/
-# Served by https://github.com/dock0/pubkeys
 # $(date)
 "
 
+export BUILD_DIR=_build
+rm -rf "$BUILD_DIR"
+mkdir "$BUILD_DIR"
+
 function build_index() {
-    echo "# $(basename $(pwd)) key set" > index.txt
-    echo "$MESSAGE" >> index.txt
+    set_name="$(basename $(pwd))"
+    set_dir="../${BUILD_DIR}/${set_name}"
+    set_index="${set_dir}/index.txt"
+    mkdir -p "$set_dir"
+    echo "# ${set_name} key set" > $set_index
+    echo "$MESSAGE" >> $set_index
     find -L . -type f -not -name '*\.*' -exec cat {} \; \
         | awk '{print length, $0}' \
         | sort -n \
         | cut -d " " -f2- \
-        | tee -a index.txt
+        | tee -a $set_index
 }
 export -f build_index
 
-find -L . -mindepth 1 -type d | xargs -n1 -I{} sh -c "cd {} && build_index"
+find -L . -mindepth 1 -maxdepth 1 -type d ! -path './.git' ! -path './_build' \
+    | xargs -n1 -I{} sh -c "cd {} && build_index"
 
-ln -s default/index.txt index.txt
+ln -s "default/index.txt" "$BUILD_DIR/index.txt"
